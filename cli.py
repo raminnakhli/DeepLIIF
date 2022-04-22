c7d35a1f3d4d217ae51d0a754884c3a64e07b6bc
raminnakhli <raminnakhli@gmail.com>
2022-04-21 19:02:15 -0700

    fix bugs

--- cli.py
+++ cli.py
@@ -510,7 +510,7 @@ def test(input_dir, output_dir, tile_size, model_dir, mask_dir=None):
 
             # Save the segmentation mask
             segmentation = np.array(images['SegRefined'])
-            segmentation = np.concatenate((segmentation[:, :, 0], 2 * segmentation[:, :, 2]))
+            segmentation = segmentation[:, :, 0] + 2 * segmentation[:, :, 2]
             if mask_dir is not None:
                 mask = Image.open(os.path.join(mask_dir, filename.replace('.' + filename.split('.')[-1], '.png')))
                 mask = np.array(mask.convert('L'))
